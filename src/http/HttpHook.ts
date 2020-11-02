96338307dabb7c2d2179e43776baa432eb4c8634
Dori Lahav Waisberg <dori.lahav@gmail.com>
2020-11-02 23:51:07 +0200

    feat(useHttp): create basic http hook

--- src/http/HttpHook.ts
+++ src/http/HttpHook.ts
@@ -1,15 +1,43 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
-interface HttpHook {
-  get: <T>(url: string) => Promise<T>
+interface HttpHookProps {
+  axiosInstance?: AxiosInstance;
 }
 
-export const useHttp = (): HttpHook => {
+interface HttpHook {
+  get: <T>(url: string) => Promise<T>;
+  delete: <T>(url: string) => Promise<T>;
+  post: <TBody, TResponse>(url: string, body: TBody) => Promise<TResponse>;
+  put: <TBody, TResponse>(url: string, body: TBody) => Promise<TResponse>;
+  patch: <TBody, TResponse>(url: string, body: TBody) => Promise<TResponse>;
+}
+
+export const useHttp: SomeHooks.Hook<HttpHookProps, HttpHook> = ({axiosInstance = axios} = {}) => {
   const get = <T>(url: string) =>
-    axios.get<T>(url)
+    axiosInstance.get<T>(url)
+      .then(response => response.data);
+  
+  const _delete = <T>(url: string) =>
+    axiosInstance.delete<T>(url)
+      .then(response => response.data);
+
+  const post = <TBody, TResponse>(url: string, body: TBody) =>
+    axiosInstance.post<TResponse>(url, body)
+      .then(response => response.data);
+      
+  const put = <TBody, TResponse>(url: string, body: TBody) =>
+    axiosInstance.put<TResponse>(url, body)
+      .then(response => response.data);
+
+  const patch = <TBody, TResponse>(url: string, body: TBody) =>
+    axiosInstance.patch<TResponse>(url, body)
       .then(response => response.data);
 
   return {
-    get
+    get,
+    delete: _delete,
+    post,
+    put,
+    patch
   };
 };
