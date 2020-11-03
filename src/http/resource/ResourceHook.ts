480ccfafa32375f98420dfb229423aa77fc0a49b
Dori Lahav Waisberg <dori.lahav@gmail.com>
2020-11-03 22:20:16 +0200

    feat(ResourceHook): simplified resource hook

--- src/http/resource/ResourceHook.ts
+++ src/http/resource/ResourceHook.ts
@@ -4,16 +4,15 @@ interface HttpResourceHookProps extends HttpHookProps {
   url: string;
 }
 
-interface HttpResourceHook<T, TId = string> {
-  getAll: () => Promise<T[]>;
-  getById: (id: TId) => Promise<T>;
-  create: (entity: T) => Promise<T | undefined>;
-  delete: (id: TId) => Promise<T | undefined>;
-  update: (id: TId, updates: Partial<T>) => Promise<T | undefined>;
-  replace: (id: TId, newEntity: T) => Promise<T | undefined>;
+interface HttpResourceHook<TViewModel, TId = string> {
+  getAll: () => Promise<TViewModel[]>;
+  getById: (id: TId) => Promise<TViewModel>;
+  create: (entity: TViewModel) => Promise<TViewModel>;
+  delete: (id: TId) => Promise<TViewModel>;
+  update: (newEntity: TViewModel) => Promise<TViewModel>;
 }
 
-export const useHttpResource = <T, TId = string>({url, ...httpHookProps} = {} as HttpResourceHookProps): HttpResourceHook<T, TId> => {
+export const useHttpResource = <TViewModel, TId = string>({url, ...httpHookProps} = {} as HttpResourceHookProps): HttpResourceHook<TViewModel, TId> => {
   const http = useHttp(httpHookProps);
 
   if (!url) {
@@ -21,29 +20,25 @@ export const useHttpResource = <T, TId = string>({url, ...httpHookProps} = {} as
   }
 
   const getAll = () =>
-    http.get<T[]>(url);
-  
-  const getById = (id: TId) =>
-    http.get<T>(`${url}/${id}`);
+    http.get<TViewModel[]>(url);
 
-  const create = (entity: T) =>
-    http.post<T | undefined, T>(url, entity);
+  const getById = (id: TId) =>
+    http.get<TViewModel>(`${url}/${id}`);
+
+  const create = (entity: TViewModel) =>
+    http.post<TViewModel, TViewModel>(url, entity);
 
   const _delete = (id: TId) =>
-    http.delete<T | undefined>(`${url}/${id}`);
+    http.delete<TViewModel>(`${url}/${id}`);
 
-  const update = (id: TId, updates: Partial<T>) =>
-    http.patch<T | undefined, Partial<T>>(`${url}/${id}`, updates);
-
-  const replace = (id: TId, newEntity: T) =>
-    http.put<T | undefined, T>(`${url}/${id}`, newEntity);
+  const update = (newEntity: TViewModel) =>
+    http.put<TViewModel, TViewModel>(url, newEntity);
 
   return {
     getAll,
     getById,
     create,
     delete: _delete,
-    update,
-    replace
+    update
   };
 };
