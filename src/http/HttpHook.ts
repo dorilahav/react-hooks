58df6786270511cd5e05b44f4dce971644e38faf
Dori Lahav Waisberg <dori.lahav@gmail.com>
2020-11-03 22:57:40 +0200

    feat(HttpHook): add support for interceptors

--- src/http/HttpHook.ts
+++ src/http/HttpHook.ts
@@ -1,8 +1,22 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
+import {useEffect} from 'react';
+
+export type AxiosInterceptor<T> =
+  [(value: T) => T | Promise<T>] |
+  [
+    (value: T) => T | Promise<T>,
+    (error: any) => any
+  ];
+
+export interface AxiosInterceptors {
+  request?: [AxiosInterceptor<AxiosRequestConfig>],
+  response?: [AxiosInterceptor<AxiosResponse>]
+}
 
 export interface HttpHookProps {
   axiosInstance?: AxiosInstance;
   axiosConfig?: AxiosRequestConfig;
+  interceptors?: AxiosInterceptors;
 }
 
 export interface HttpHook {
@@ -13,13 +27,26 @@ export interface HttpHook {
   patch: <TResponse = any, TBody = any>(url: string, body: TBody) => Promise<TResponse>;
 }
 
-export const useHttp = ({axiosInstance, axiosConfig} = {} as HttpHookProps): HttpHook => {
+export const useHttp = ({axiosInstance, axiosConfig, interceptors} = {} as HttpHookProps): HttpHook => {
   const instance = axiosInstance ?? axios.create(axiosConfig);
+
+  const registerInterceptors = () => {
+    interceptors?.request?.map(interceptor => instance.interceptors.request.use(...interceptor));
+    interceptors?.response?.map(interceptor => instance.interceptors.response.use(...interceptor));
+  }
+
+  useEffect(() => {
+    if (!instance) {
+      return
+    }
+
+    registerInterceptors();
+  }, [instance]);
 
   const get = <T>(url: string) =>
     instance.get<T>(url)
       .then(response => response.data);
-  
+
   const _delete = <T>(url: string) =>
     instance.delete<T>(url)
       .then(response => response.data);
@@ -27,7 +54,7 @@ export const useHttp = ({axiosInstance, axiosConfig} = {} as HttpHookProps): Htt
   const post = <TResponse, TBody>(url: string, body: TBody) =>
     instance.post<TResponse>(url, body)
       .then(response => response.data);
-      
+
   const put = <TResponse, TBody>(url: string, body: TBody) =>
     instance.put<TResponse>(url, body)
       .then(response => response.data);
